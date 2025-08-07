39fe98be6c6615cda84dd3638b31091da4a16573
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-07 22:32:30 +0600

    function intro

---
+++ function-intro.js
@@ -0,0 +1,31 @@
+const money = 45;
+
+// fucntion declare
+function fanOfKor(){
+    console.log('bosa theke ute dara');
+    console.log('work towards the switch');
+    console.log('click the switch to  off the Fan');
+}
+
+// call the function 
+fanOfKor();
+
+
+// function name (){
+//
+// }
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('toothpaste lagao');
+    console.log('2 min ghoso and ghoso and fena felo');
+}
+
+// call fthe function 
+brushTeeth();
+console.log('---------------');
+console.log('---------------');
+console.log('---------------');
+console.log('tom tom tomato');
+
+brushTeeth();
+
