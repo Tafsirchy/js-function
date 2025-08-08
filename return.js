ee51a615f73676730fcf5a7fd234daf7652aaf39
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-08 10:13:50 +0600

    return

---
+++ return.js
@@ -0,0 +1,22 @@
+function tenTimes(num){
+    const result = num * 10;
+    return result;
+}
+
+function cutHalf(num){
+    const result = num / 2;
+    return result;
+}
+
+
+const output1 = tenTimes(10);
+console.log(output1);
+
+const bigNumber1 = tenTimes(99);
+console.log(bigNumber1);
+
+const output2 = cutHalf(45);
+console.log(output2);
+
+const bigNumber2 = cutHalf(999999);
+console.log(bigNumber2);
